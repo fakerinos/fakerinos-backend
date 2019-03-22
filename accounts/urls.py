6b6207733277c4609d55c673673755a1baf5d6ad
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-03-22 17:26:38 +0800

    Migrations fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
-from allauth.account.views import confirm_email
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
     path('', include('allauth.urls')),
-    path('register/account-confirm-email/<key>/', confirm_email),
     path('register/', include('rest_auth.registration.urls')),
 ]
