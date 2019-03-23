18a9586f56d5c1f5fdc16964d9858c159310c792
Zile <14sczile@gmail.com>
2019-03-23 18:59:56 +0800

    Adjusted articles/router

--- articles/urls.py
+++ articles/urls.py
@@ -3,9 +3,8 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('articles', views.ArticleViewSet)
 router.register('decks', views.DeckViewSet)
-router.register('', views.ArticleViewSet, 'articles')
+router.register('articles', views.ArticleViewSet, 'articles')
 
 urlpatterns = [
     path('', include(router.urls)),
