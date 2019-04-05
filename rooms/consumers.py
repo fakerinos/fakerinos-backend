4851c5975d8043f13f1aa83a93a9f0de6d99f528
Zile <14sczile@gmail.com>
2019-04-05 18:37:01 +0800

    Updated websocket for rooms API

--- rooms/consumers.py
+++ rooms/consumers.py
@@ -1,25 +1,45 @@
-from channels.generic.websocket import JsonWebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer, WebsocketConsumer
 from asgiref.sync import async_to_sync
 from .models import Room
 import logging
-
+from articles.models import Article
+import json
+import channels.layers
+import websockets
+from django.core import serializers
 
 class RoomConsumer(JsonWebsocketConsumer):
     def websocket_connect(self, message):
         self.room_id = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'room_%s' % self.room_id
+        # Room.objects.create(pk=1)
+        # Article.objects.create(headline="test1")
+        # Article.objects.create(headline="test2")
+        # room = Room.objects.get(pk=1)
+        # async_to_sync(channels.layers.get_channel_layer(alias="game-consumer").send(
+        #     "game-consumer",
+        #     {
+        #         "type": 'websocket.connect'
+        #     },
+        # ))
         room = Room.objects.get(id=self.room_id)
         user = self.scope['user']
         players = room.players.all()
-        if user.is_authenticated and user not in players and len(players) < room.max_players:
-            logging.info("User {} joined room {}".format(self.scope['user'].pk, room.pk))
-            user.room = room
-            user.save()
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        if user.is_authenticated and user.profile not in players and len(players) < room.max_players:
+            logging.info("User {} joined room {}".format(self.scope['user'].id, room.id))
+            user.profile.room = room
+            user.profile.save()
             self.accept()
-        elif user in players:
+
+        elif user.profile in players:
             self.accept()
         else:
             self.close()
+
 
     def disconnect(self, code):
         user = self.scope['user']
@@ -29,3 +49,55 @@ class RoomConsumer(JsonWebsocketConsumer):
             user.save()
             logging.info("User {} left room {}".format(user.pk, room.pk))
             room.delete_if_empty()
+
+    def receive_json(self, content, **kwargs):
+        # MUST BE double quotes !!! ""
+        user = self.scope['user']
+        message = content["message"]
+        cont = json.loads(message)
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            cont
+        )
+
+    def article_request(self, event):
+        article_pk = event["input"]
+        if Article.objects.filter(pk=article_pk).exists():
+            article = serializers.serialize("json", [Article.objects.get(pk=article_pk)])
+        else:
+            article = None
+        self.send(text_data=json.dumps({
+            'message': article
+        }))
+
+    def update_score(self):
+        user = self.scope['user']
+        #TODO update score
+
+
+    def submit_final_score(self):
+        user = self.scope['user']
+        #TODO submit score
+
+
+
+class GameConsumer(WebsocketConsumer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        async_to_sync(self.channel_layer.group_add)(
+            "gameconsumer",
+            self.channel_name
+        )
+
+    def websocket_connect(self, message):
+        """self.game_group_name = 'game_%s'"""
+        self.accept()
+
+
+
+    def websocket_disconnect(self, message):
+        """
+        # async_to_sync(self.channel_layer.send(
+        #
+        # ))"""
---
+++ rooms/tests/test_consumer.py
@@ -0,0 +1,56 @@
+import pytest
+from asgiref.sync import sync_to_async
+from channels.routing import URLRouter, ProtocolTypeRouter
+from channels.testing import HttpCommunicator, ApplicationCommunicator, WebsocketCommunicator
+from fakerinos.routing import application
+from django.contrib.auth import get_user_model
+from mixer.backend.django import mixer
+from django.http import request
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+User = get_user_model()
+
+
+@pytest.mark.django_db(transaction=True)
+class testConsumers(APITestCase):
+    def setUp(self):
+        # create token
+        self.user = mixer.blend(User)
+        self.token = Token.objects.create(user=self.user)
+        self.headers = [(b'authorization', "Token {}".format(self.token.key).encode())]
+        self.communicator = WebsocketCommunicator(application, "ws/rooms/room_1/", headers=self.headers)
+
+    @pytest.mark.asyncio
+    async def login(self):
+        connected, subprotocol = await self.communicator.connect()
+        return connected, subprotocol
+
+    @pytest.mark.asyncio
+    async def test_login(self):
+        connected, _ = await self.login()
+        assert connected
+
+    @pytest.mark.asyncio
+    async def test_send_text_data(self):
+        connected, _ = await self.login()
+        await self.communicator.send_to(text_data="hello")
+        response = await self.communicator.receive_from()
+        assert "hello" == await self.communicator.receive_from()
+        # # Close
+        # await self.communicator.disconnect()
+
+    @pytest.mark.asyncio
+    async def test_data_base(self):
+        print("hello")
+        _, _ = self.login()
+        await self.communicator.send_json_to({'type': 'article_request', 'input': 0})
+        response = await self.communicator.receive_from()
+        print(response)
+        self.assertEqual(response.status, status.HTTP_200_OK)
+        assert response.status == status.HTTP_200_OK
+        assert 0 == response['headline']
+
+
+
