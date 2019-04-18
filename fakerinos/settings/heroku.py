0419e9cdd66e8f8eb31db6d3ca0e6d3b9edca8a5
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-04-18 23:28:56 +0800

    Separate Static and Media

--- fakerinos/settings/heroku.py
+++ fakerinos/settings/heroku.py
@@ -19,12 +19,16 @@ CSRF_COOKIE_SECURE = True
 
 # AWS Storage
 INSTALLED_APPS += ['storages']
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_AUTO_CREATE_BUCKET = True
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
+# S3 Static Files
+STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
+STATICFILES_STORAGE = 'fakerinos.storage_backends.StaticStorage'
+# S3 Public Media Files
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
+DEFAULT_FILE_STORAGE = 'fakerinos.storage_backends.MediaStorage'
 
 django_heroku.settings(locals(), databases=False, test_runner=False, allowed_hosts=False, secret_key=False)
