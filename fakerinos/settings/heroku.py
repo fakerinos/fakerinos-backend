1978439a487ae00fd575a2ebf8862f132ecebbc3
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-04-19 00:35:57 +0800

    Trying another thing

--- fakerinos/settings/heroku.py
+++ fakerinos/settings/heroku.py
@@ -22,16 +22,18 @@ INSTALLED_APPS += ['storages']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_AUTO_CREATE_BUCKET = True
 AWS_DEFAULT_ACL = None
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 # S3 Static Files
-STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STATICFILES_STORAGE = 'fakerinos.storage_backends.StaticStorage'
 # S3 Public Media Files
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 DEFAULT_FILE_STORAGE = 'fakerinos.storage_backends.MediaStorage'
 
-django_heroku.settings(locals(), databases=False, test_runner=False, allowed_hosts=False, secret_key=False)
+django_heroku.settings(locals(), databases=False, test_runner=False, allowed_hosts=False, secret_key=False,
+                       staticfiles=False)
