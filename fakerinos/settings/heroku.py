7c5692c7091356643d5b32ef6cb7493c75883230
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-04-18 21:42:16 +0800

    AWS S3 Storage Setup

--- fakerinos/settings/heroku.py
+++ fakerinos/settings/heroku.py
@@ -17,4 +17,14 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+# AWS Storage
+INSTALLED_APPS += ['storages']
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_AUTO_CREATE_BUCKET = True
+AWS_DEFAULT_ACL = 'public-read'
+
 django_heroku.settings(locals(), databases=False, test_runner=False, allowed_hosts=False, secret_key=False)
