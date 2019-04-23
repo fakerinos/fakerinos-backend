d788c7df1ab10cb40bcc2e5efc53f3a158b3e2e3
Zile <14sczile@gmail.com>
2019-04-23 12:56:18 +0800

    working

--- rooms/tests/test_consumer.py
+++ rooms/tests/test_consumer.py
@@ -9,48 +9,56 @@ from django.http import request
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from rest_framework import status
+from ..consumers import RoomConsumer
 
 User = get_user_model()
 
 
-@pytest.mark.django_db(transaction=True)
-class testConsumers(APITestCase):
-    def setUp(self):
-        # create token
-        self.user = mixer.blend(User)
-        self.token = Token.objects.create(user=self.user)
-        self.headers = [(b'authorization', "Token {}".format(self.token.key).encode())]
-        self.communicator = WebsocketCommunicator(application, "ws/rooms/room_1/", headers=self.headers)
+# @pytest.mark.django_db(transaction=True)
+# class testConsumers(APITestCase):
+#     def setUp(self):
+#         # create token
+#         self.user = mixer.blend(User)
+#         self.token = Token.objects.create(user=self.user)
+#         self.headers = [(b'authorization', "Token {}".format(self.token.key).encode())]
+#         self.communicator = WebsocketCommunicator(application, "ws/rooms/", headers=self.headers)
+#
+#     @pytest.mark.asyncio
+#     def login(self):
+#         connected, subprotocol = await self.communicator.connect()
+#         return connected, subprotocol
+#
+#     @pytest.mark.asyncio
+#     def test_login(self):
+#         connected, _ = await self.login()
+#         assert connected
+#
+#     @pytest.mark.asyncio
+#     def test_send_text_data(self):
+#         connected, _ = await self.login()
+#         await self.communicator.send_to(text_data="hello")
+#         response = await self.communicator.receive_from()
+#         assert "hello" == await self.communicator.receive_from()
+#         # # Close
+#         # await self.communicator.disconnect()
+#
+#     @pytest.mark.asyncio
+#     def test_data_base(self):
+#         print("hello")
+#         _, _ = self.login()
+#         await self.communicator.send_json_to({'type': 'article_request', 'input': 0})
+#         response = await self.communicator.receive_from()
+#         print(response)
+#         self.assertEqual(response.status, status.HTTP_200_OK)
+#         assert response.status == status.HTTP_200_OK
+#         assert 0 == response['headline']
 
-    @pytest.mark.asyncio
-    async def login(self):
-        connected, subprotocol = await self.communicator.connect()
-        return connected, subprotocol
-
-    @pytest.mark.asyncio
-    async def test_login(self):
-        connected, _ = await self.login()
-        assert connected
-
-    @pytest.mark.asyncio
-    async def test_send_text_data(self):
-        connected, _ = await self.login()
-        await self.communicator.send_to(text_data="hello")
-        response = await self.communicator.receive_from()
-        assert "hello" == await self.communicator.receive_from()
-        # # Close
-        # await self.communicator.disconnect()
-
-    @pytest.mark.asyncio
-    async def test_data_base(self):
-        print("hello")
-        _, _ = self.login()
-        await self.communicator.send_json_to({'type': 'article_request', 'input': 0})
-        response = await self.communicator.receive_from()
-        print(response)
-        self.assertEqual(response.status, status.HTTP_200_OK)
-        assert response.status == status.HTTP_200_OK
-        assert 0 == response['headline']
-
-
-
+async def test_connect():
+    user = mixer.blend(User)
+    token = Token.objects.create(user=user)
+    headers = [(b'authorization', "Token {}".format(token.key).encode())]
+    communicator = WebsocketCommunicator(RoomConsumer, '/ws/rooms/', headers)
+    connected, subprotocol = await communicator.connect()
+    print("notice me {}".format(connected))
+    assert connected
+    await communicator.disconnect()
